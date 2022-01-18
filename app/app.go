9c199b093c1f3a7f9aa8ef28c6b9d0c439ea993a
joepaulmartin <joepaul.martin1448@gmail.com>
2022-01-18 16:01:09 +0530

    added yaml file
    added yaml file and tested

--- app/app.go
+++ app/app.go
@@ -2,18 +2,75 @@ package app
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
+	"os"
 	"time"
 
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 	"github.com/jmoiron/sqlx"
-	_ "github.com/go-sql-driver/mysql"
 	"github.com/joepaul-martin/banking-auth/domain"
+	"github.com/joepaul-martin/banking-auth/errs"
 	"github.com/joepaul-martin/banking-auth/service"
+	"gopkg.in/yaml.v3"
 )
 
+const configFileName = "config.yaml"
+
+type serverData struct {
+	Server struct {
+		ServerAddress string `yaml:"serverAddress"`
+		ServerPort    string `yaml:"serverPort"`
+	} `yaml:"server"`
+}
+
+type databaseData struct {
+	MySql struct {
+		DB       string `yaml:"db"`
+		Port     string `yaml:"port"`
+		User     string `yaml:"user"`
+		DbName   string `yaml:"dbName"`
+		Password string `yaml:"password"`
+	} `yaml:"mysql"`
+}
+
+func readServerConfig(fileName string) (*serverData, *errs.AppError) {
+	buf, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		return nil, errs.NewUnexpectedError(fmt.Sprintf("Error while reading the config file: %s, error : %s", fileName, err.Error()))
+	}
+	data := &serverData{}
+	err = yaml.Unmarshal(buf, data)
+	if err != nil {
+		return nil, errs.NewUnexpectedError(fmt.Sprintf("Error while unmarshalling object: %s", err.Error()))
+	}
+	return data, nil
+}
+
+func readDBConfig(fileName string) (*databaseData, *errs.AppError) {
+	buf, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		return nil, errs.NewUnexpectedError(fmt.Sprintf("Error while reading the config file: %s, error : %s", fileName, err.Error()))
+	}
+	data := &databaseData{}
+	err = yaml.Unmarshal(buf, data)
+	if err != nil {
+		return nil, errs.NewUnexpectedError(fmt.Sprintf("Error while unmarshalling object: %s", err.Error()))
+	}
+	return data, nil
+}
+
 func getDbClient() *sqlx.DB {
-	client, err := sqlx.Open("mysql", "root:acer@dts@tcp(localhost:3306)/banking")
+	dbConfig, appErr := readDBConfig(configFileName)
+	if appErr != nil {
+		log.Fatal(appErr)
+	}
+	password := os.Getenv("DBPASSWORD")
+
+	dataSourceName := fmt.Sprintf("%s:%s@tcp(%s)/%s", dbConfig.MySql.User, password, dbConfig.MySql.Port, dbConfig.MySql.DbName)
+	client, err := sqlx.Open(dbConfig.MySql.DB, dataSourceName)
 	if err != nil {
 		panic(err)
 	}
@@ -33,8 +90,12 @@ func Start() {
 
 	router.HandleFunc("/login", lh.login).Methods(http.MethodPost)
 
-	fmt.Printf("Started the application")
+	// starting server
+	serverConfig, err := readServerConfig(configFileName)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	http.ListenAndServe("localhost:8001", router)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf("%s:%s", serverConfig.Server.ServerAddress, serverConfig.Server.ServerPort), router))
 
 }
--- go.mod
+++ go.mod
@@ -7,4 +7,5 @@ require (
 	github.com/go-sql-driver/mysql v1.6.0
 	github.com/gorilla/mux v1.8.0
 	github.com/jmoiron/sqlx v1.3.4
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b
 )
